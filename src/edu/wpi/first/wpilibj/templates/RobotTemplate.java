11dc22bd3d3d5243b0521a840277d48e9ba959a7
Adam Carmichael <carneeki@carneeki.net>
2013-01-21 02:25:27 +1100

    javadoc all the things
    also, split VIC corrections into reusable method.

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -21,27 +21,99 @@ import edu.wpi.first.wpilibj.Victor;
  */
 public class RobotTemplate extends SimpleRobot
 {
+  /**
+   * Driver joystick deadzone for x-axis. Default 0.05
+   */
+  static float JOY_DRV_DEAD_X = (float) 0.05;
   
-  RobotDrive rdDrive = new RobotDrive(9,10);
+  /**
+   * Driver joystick deadzone for y-axis. Default 0.05
+   */
+  static float JOY_DRV_DEAD_Y = (float) 0.05;
   
-  Joystick joystickLeft = new Joystick(1);
-  Joystick joystickRight = new Joystick(2);
+  /**
+   * Operator joystick deadzone for x-axis. Default 0.05
+   */
+  static float JOY_OPR_DEAD_X = (float) 0.05;
   
-  /*
+  /**
+   * Operator joystick deadzone for y-axis. Default 0.05
+   */
+  static float JOY_OPR_DEAD_Y = (float) 0.05;
+  
+  /**
+   * Delay for timer events. Default = 0.01 (100Hz)
+   */
+  static float TIMER_DELAY = (float) 0.01; 
+  
+  /**
+   * Maximum speed of VIC. Default = 1.00
+   */
+  static float VIC_SPEED_MAX = (float) 1.00;
+  
+  /**
+   * Minimum speed of VIC. Default = 0.05
+   */
+  static float VIC_SPEED_MIN = (float) 0.05;
+  
+  /**
+   * Stop speed of VIC. Default = 0
+   */
+  static float VIC_SPEED_ZERO = (float) 0;
+  
+  /**
+   * RobotDrive.
+   * TODO: extend robot drive to use 3 wheel holonomic
+   */
+  RobotDrive rdDrive = new RobotDrive(9, 10);
+  
+  /**
+   * Joystick for the driver.
+   */
+  Joystick joystickDriver = new Joystick(1);
+  
+  /**
+   * Joystick for the operator.
+   */
+  Joystick joystickOperator = new Joystick(2);
+  
+  /**
+   * Velocity for motors A, B, C. Valid velocity range is between -1.00 and 1.00
+   * 
    * Motor positions: 
    *          A
-   *          |=vX
+   *          | = vX
    *          .
    * 
    *      C       B
-   *      |       |=-vX/2 - sqrt(3)/2 * vY
-   *      |=-vX/2 + sqrt(3)/2 * vY
+   *      |       | = -vX/2 - sqrt(3)/2 * vY
+   *      |
+   *      | = -vX/2 + sqrt(3)/2 * vY
+   * 
    */
   double vA, vB, vC = 0; // velocity for motors A, B, C
   
-  Victor motorA = new Victor(1,1);
-  Victor motorB = new Victor(1,2);
-  Victor motorC = new Victor(1,3);
+  /**
+   * Motor controller objects for A, B, C drive motors.
+   */
+  Victor motorA = new Victor(1, 1);
+  Victor motorB = new Victor(1, 2);
+  Victor motorC = new Victor(1, 3);
+  
+  /**
+   * Motor controller objects for rest of robot:
+   */
+  Victor indexer = new Victor(1, 4);
+  Victor shooterA = new Victor(1, 5);
+  Victor shooterB = new Victor(1 ,6);
+  Victor climber = new Victor(1,7);
+
+  
+  //////////////////////////////////////////////////////////////////////////////
+  //
+  // Place methods here
+  //  
+  //////////////////////////////////////////////////////////////////////////////
   
   /**
    * This function is called once each time the robot enters autonomous mode.
@@ -57,54 +129,88 @@ public class RobotTemplate extends SimpleRobot
   public void operatorControl()
   {
     rdDrive.setSafetyEnabled(true);
-    
-    while( isOperatorControl() && isEnabled() )
+
+    while (isOperatorControl() && isEnabled())
     {
-      this.VectorDrive(joystickLeft.getX(), joystickLeft.getY());
-      Timer.delay(0.01);
-    }
-  }
+      drive(joystickDriver.getX(), joystickDriver.getY());
+
+      // DO NOT PLACE ANYTHING AFTER THIS LINE IN operatorControl() !!
+      Timer.delay(TIMER_DELAY);
+    } // while (isOperatorControl() && isEnabled())
+  } // public void operatorControl()
 
   /**
    * This function is called once each time the robot enters test mode.
    */
   public void test()
   {
-  }
-  
+  } // public void test()
+
   /**
-   * concept based on Vector_Drive from CMU implementation at
-   * http://www.cs.cmu.edu/~pprk/Download/robot.cpp
-   * and
-   * http://www.rjmcnamara.com/2012/06/holonomic-platforms-robotc/
+   * control motor drive train concept based on Vector_Drive from CMU
+   * implementation at http://www.cs.cmu.edu/~pprk/Download/robot.cpp and
+   * http://www.rjmcnamara.com/VIC_SPEED_MIN12/06/holonomic-platforms-robotc/
+   *
    * @param vX x position of joystick
    * @param vY y pos of joystick
    * @author Adam Carmichael <carneeki@carneeki.net>
-   * @since 2013-01-19 12:30AEDT
+   * @since VIC_SPEED_MIN13-01-19 12:30AEDT
    */
-  private void VectorDrive(double vX, double vY)
+  private void drive(double vX, double vY)
   {
-    
     // set MotorA
     vA = vX;
-    if(vA < 20  && vA >    0) { vA =   0; } // Set Minimum MotorA's Forward Power
-    if(vA < 0   && vA >  -20) { vA =   0; } // Set Minimum MotorA's Reverse Power
-    if(vA > 100 || vA < -100) { vA = 100; } // Set Maximum Motor Power Level at 100
-    
-    // set MotorB
-    vB = (-vX / 2) - ( Math.sqrt(3) /2 * vY );
-    if(vB <  20 && vB >    0) { vB =   0; } // Set Minimum MotorB's Forward Power
-    if(vB <   0 && vB >  -20) { vB =   0; } // Set Minimum MotorB's Reverse Power
-    if(vB > 100 || vB < -100) { vB = 100; } // Set Maximum Motor Power Level at 100
-    
-    // set MotorC
-    vC = (-vX / 2) + ( Math.sqrt(3) /2 * vY );
-    if(vC <  20 && vC >    0) { vC =   0; } // Set Minimum MotorC's Forward Power
-    if(vC <   0 && vC >  -20) { vC =   0; } // Set Minimum MotorC's Reverse Power
-    if(vC > 100 || vC < -100) { vC = 100; } // Set Maximum Motor Power Level at 100
-    
+    vB = (-vX / 2) - (Math.sqrt(3) / 2 * vY);
+    vC = (-vX / 2) + (Math.sqrt(3) / 2 * vY);
+
+    // do deadzone corrections
+    vA = victorCorrection(vA);
+    vB = victorCorrection(vB);
+    vC = victorCorrection(vC);
+
     motorA.set(vA);
     motorB.set(vB);
     motorC.set(vC);
-  }
-}
+  } // private void drive(double vX, double vY)
+
+  /**
+   * correct the values sent to VIC such that deadzones and speed boundaries are
+   * respected
+   *
+   * @author Adam Carmichael <carneeki@carneeki.net>
+   * @since 2013-01-19 18:35 AEDT
+   * @param vN double speed
+   * @return double corrected value
+   */
+  private float victorCorrection(double speed)
+  {
+    // in deadzone > 0
+    if ((speed > VIC_SPEED_ZERO) && (speed < VIC_SPEED_MIN))
+    {
+      return VIC_SPEED_ZERO;
+    }
+
+    // in deadzone < 0
+    if ((speed > -VIC_SPEED_MIN) && (speed < VIC_SPEED_ZERO))
+    {
+      return -VIC_SPEED_ZERO;
+    }
+
+    // minimum boundary exceeded
+    if (speed < -VIC_SPEED_MAX)
+    {
+      return -VIC_SPEED_MAX;
+    }
+
+    // maximum boundary exceeded
+    if (speed > VIC_SPEED_MAX)
+    {
+      return VIC_SPEED_MAX;
+    }
+
+    // something went badly wrong
+    // TODO: throw an exception, or put a warning on the dashboard...
+    return VIC_SPEED_ZERO;
+  } // private float victorCorrection(double speed)
+  
+} // public class RobotTemplate extends SimpleRobot
